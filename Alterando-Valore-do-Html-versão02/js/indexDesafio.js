e77236e4a9d516e65f7d7778da8cd338edc31a58
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2023-02-26 14:28:18 -0300

    fazendo duas funções para poder apagar e assender as lampadas em um arquivo separado

---
+++ Alterando-Valore-do-Html-versão02/js/indexDesafio.js
@@ -0,0 +1,10 @@
+function Ligada(){
+   console.log('está ligada')
+   document.getElementById('ligada').src='img/luzLigada.gif'
+}
+ 
+
+function Apagada(){
+   console.log('está Apagada');
+   document.getElementById('ligada').src='img/luzApagada.gif'
+}
